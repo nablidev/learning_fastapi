12920943649a98db6ea3d13bcb03a0fffa9a3087
devgarage <dhianabli@outlook.com>
2022-02-17 10:54:53 +0100

    add first route

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,7 +1,15 @@
-#config.py
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
 
 class Settings:
     PROJECT_NAME: str = "Job Board"
     PROJECT_VERSION: str = "1.0.0"
+#    DATABASE_URL =  "sqlite:///test.db"
+
 
 settings = Settings()
--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
-#main.py
 import uvicorn
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles #new
+from fastapi.staticfiles import StaticFiles
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
+from db.session import engine
+from db.base import Base
 
 
 def include_router(app):
@@ -14,10 +15,15 @@ def configure_static(app):
 	app.mount("/static", StaticFiles(directory="backend/static"), name="static")
 
 
+def create_tables():
+	Base.metadata.create_all(bind=engine)
+
+
 def start_application():
 	app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 	include_router(app)
 	configure_static(app)
+	create_tables()
 	return app
 
 
